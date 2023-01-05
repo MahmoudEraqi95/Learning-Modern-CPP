1f26c5ef1c504a211f8e364dfb35729061cda2a7
Eraqi <mahmouderaqi095@gmail.com>
2023-01-05 11:29:31 +0200

    worked a little with list and auto

--- template.cpp
+++ template.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<list>
 using namespace std;
 
 template <class T>
@@ -6,6 +7,13 @@ T maxOf(T a, T b){
     return (a>b? a: b);
 }
 int main(){
+    list<int> l = {1,2,3,4,5,6,7,8,9,10};
+    auto it = l.begin();
+    while(*++it != 5 );
+    l.insert(it, 145);
+    for(auto i: l)
+        cout<<i;
+    cout<<endl;    
     cout<<maxOf<int>(5, 6)<<endl;
     return 0;
 }
